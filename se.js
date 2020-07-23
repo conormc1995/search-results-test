af8ca471e4b07f30c9997a79c0e6c37f5f8c56db
conormc1995 <conormcloughlin64@gmail.com>
2020-07-23 08:06:31 +0100

    1

--- se.js
+++ se.js
@@ -26,10 +26,7 @@ async function mainFunction() {
 }
 
 async function getActualResults(queries) {
-  const browser = await playwright["chromium"].launch({
-    headless: true,
-    args: ["--no-sandbox"],
-  });
+  const browser = await playwright["chromium"].launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] };
 
   const context = await browser.newContext();
   const page = await context.newPage();
