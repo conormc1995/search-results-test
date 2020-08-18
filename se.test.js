211447ef0ddf4e885222a906627030098dcda7db
conormc1995 <48835836+conormc1995@users.noreply.github.com>
2020-08-18 09:08:02 +0100

    jest

--- se.test.js
+++ se.test.js
@@ -1,3 +1,6 @@
+const checkExpectedResultsArePresent = require("./se");
+
+//expect there to be 0 missing results
 test("Test for missing expected results", async () => {
-  expect(actualCourseList[queryIndex]).toContain(i);
+  expect(checkExpectedResultsArePresent).toEqual([]);
 });
--- se2.js
+++ se2.js
@@ -40,9 +40,6 @@ async function mainFunction() {
   console.log(actualResultsList);
   //compare contents of array and output results to separate Google Sheet
   compareResults(idealResultsList, actualResultsList);
-
-  //check for missing results
-  checkExpectedResultsArePresent(idealResultsList, actualResultsList);
 }
 
 async function getActualResults(queries) {
@@ -202,33 +199,22 @@ async function compareResults(idealCourseList, actualCourseList) {
     }
   }
 
-  //Check for missing expected results
-  //if an expected result is missing
-  //add it to an array of missing results
-  //return missing results array
   async function checkExpectedResultsArePresent(
     idealCourseList,
     actualCourseList
   ) {
     let queryIndex;
-    let missingResults = [];
-    let missingResultsMulti = [];
 
     for (queryIndex = 0; queryIndex < numberOfQueries; queryIndex++) {
       for (i of idealCourseList[queryIndex]) {
         if (actualCourseList[queryIndex].includes(i)) {
-          console.log("is Present");
+          return 0;
         } else {
-          missingResults.push(i);
+          return i;
         }
       }
-      missingResultsMulti.push(missingResults);
     }
-
-    return missingResultsMulti;
   }
-
-  module.exports = checkExpectedResultsArePresent;
 
   await sheet.saveUpdatedCells();
 }
