1c2dff07f7245188654c03b911fb299b427b88bb
conormc1995 <48835836+conormc1995@users.noreply.github.com>
2020-08-19 17:03:47 +0100

    final

--- se.test.js
+++ se.test.js
@@ -1,7 +1,32 @@
 const se = require("./se.js");
 
+let queryArray = [
+  "digital marketing",
+  "english",
+  "accounting",
+  "law",
+  "marketing",
+  "c",
+  "microsoft",
+  "safety",
+  "iso",
+  "sales",
+  "finance",
+  "coronavirus",
+  "data science",
+  "management",
+  "mental health",
+  "food",
+  "math",
+  "health and safety",
+  "logistics",
+  "microsoft office",
+  "html",
+  "sap",
+  "health",
+];
 //expect there to be 0 missing results
 test("Test for missing expected results", async () => {
   jest.setTimeout(500000);
-  return expect(se()).resolves.toEqual([]);
+  return expect(se()).resolves.toEqual(queryArray);
 });
